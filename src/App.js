dfa56665ad35c0a390d91f6c2a4910c0994fcc3a
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-02 16:50:26 +0000

    Added movies by default

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import './App.css';
 function App() {
   return (
     <List data={[
-      { title:'tenet',
-      id: "10"},
-      {title: 'amelie',
-       id: "11"},        
+      {title: 'tenet', id:"0", ranking: 10, type:"accion", director:"Nollan"},
+      {title: 'amelie', id:"2" , ranking: 10, type:"romantic", director:"Tiersen"},      
   ]} />
   );
 }
