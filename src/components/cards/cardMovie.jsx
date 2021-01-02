9c8824434aa0d18b5a12e895800056132dd67513
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-02 15:51:41 +0000

    create movie: added style to card movie

---
+++ src/components/cards/cardMovie.jsx
@@ -0,0 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Card from 'react-bootstrap/Card';
+
+const CardMovie = function(props) {
+    return (
+        <div>
+            <Card  style={{ width: '18rem' }}>
+                <Card.Header>Ranking</Card.Header>
+                <Card.Img variant="top" src={process.env.PUBLIC_URL + '/img/' + props.title + ".jpg"} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">accion</Card.Subtitle>
+                    <Card.Text>movie director</Card.Text>
+                </Card.Body>   
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="danger" onClick={() => props.deleteClick(props.id)}>Delete</Button>
+                    <Button variant="info"onClick={() => props.editClick(props.id)}>Edit</Button>
+                </ButtonGroup>     
+            </Card>    
+        </div>
+    )
+};
+
+export default CardMovie;
