83051364421fdaa26f17d29e3195b91b41c3760d
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-02 15:51:11 +0000

    Card: rename to card movie

--- src/components/cards/card.test.jsx
+++ src/components/cards/card.test.jsx
@@ -1,28 +1,28 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 
-import Card from "./card";
+import CardMovie from "./cardMovie";
 
 test('Should render a movie title', () => {
-    render ( <Card title="tenet"/>)
+    render ( <CardMovie title="tenet"/>)
     const title = screen.getByText("tenet")
     expect(title).toBeInTheDocument();
 })
 
 test('Should render movies titles', () => {
-    render (<Card title="tenet, inception, amelie"/>)
+    render (<CardMovie title="tenet, inception, amelie"/>)
     const titles = screen.getByText('tenet, inception, amelie')
     expect(titles).toBeInTheDocument();
 })
 
 test('render a button with delete text', () => {
-    render(<Card />)
+    render(<CardMovie />)
     const deleteButton = screen.getByRole('button', {name: /delete/i})
     expect(deleteButton).toBeInTheDocument();
 })
 
 test('should calls onClick with id once when clicked', () => {
     const deleteClick = jest.fn()
-    render(<Card id="5" deleteClick={deleteClick}/>)
+    render(<CardMovie id="5" deleteClick={deleteClick}/>)
     fireEvent.click(screen.getByText(/delete/i))
     expect(deleteClick).toHaveBeenCalledTimes(1)
     expect(deleteClick).toHaveBeenCalledWith("5")
@@ -30,7 +30,7 @@ test('should calls onClick with id once when clicked', () => {
 
 test('should call editClick with id once when clicked', () => {
     const editClick = jest.fn()
-    render(<Card id="5" editClick={editClick}/>)
+    render(<CardMovie id="5" editClick={editClick}/>)
     fireEvent.click(screen.getByText('Edit'))
     expect(editClick).toHaveBeenCalledTimes(1)
     expect(editClick).toHaveBeenCalledWith("5")
